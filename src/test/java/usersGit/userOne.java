8884c2abb7714604ba511c53a2494388339916f6
Hariprasath-Prasath <hariprasathbscecs@gmail.com>
2025-04-24 15:38:21 +0530

    new changed

--- src/test/java/usersGit/userOne.java
+++ src/test/java/usersGit/userOne.java
@@ -2,4 +2,20 @@ package usersGit;
 
 public class userOne {
 
+	private void Tamil() {
+		System.out.println("10 Users");
+	}
+
+	private void english() {
+		System.out.println("20 Users");
+	}
+	private void kanadam() {
+		System.out.println("30 Users");
+	}
+	private void malayalam() {
+		System.out.println("25 Users");
+	}
+	private void hindi() {
+		System.out.println("15 Users");
+	}
 }
